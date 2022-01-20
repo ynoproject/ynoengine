a02f02ef6623a394eb898409d4703a230229443b
kekami <kekami@kekami.dev>
2022-01-20 12:40:54 -0500

    Fix possible problem with sending signatures

--- src/game_multiplayer.cpp
+++ src/game_multiplayer.cpp
@@ -139,17 +139,17 @@ namespace {
 			sha1::SHA1 checksum;
 			std::string header;
 			uint32_t digest[5];
-			char counter[8];
-			char signature[8];
+			char counter[7];
+			char signature[7];
 
 			msg_count = msg_count + 1; //increment message count
-			snprintf(counter, 8, "%06d", msg_count); //format message count
+			snprintf(counter, 7, "%06d", msg_count); //format message count
 
 			std::string hashmsg = key + secret + counter + msg; //construct string for us to hash
 
 			checksum.processBytes(hashmsg.c_str(), hashmsg.size());
 			checksum.getDigest(digest);
-			snprintf(signature, 8, "%06x", digest[0]);
+			snprintf(signature, 7, "%06x", digest[0]);
 
 			header = signature;
 			header += counter;
