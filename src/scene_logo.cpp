095cf5bddb902478573b3b57a0fc4d4284a00587
aleck099 <aleck099@qq.com>
2022-01-21 23:40:18 +0800

    Retry when fails to fetch necessary file

--- src/scene_logo.cpp
+++ src/scene_logo.cpp
@@ -127,10 +127,12 @@ void Scene_Logo::OnIndexReady(FileRequestResult*) {
 	ini->SetImportantFile(true);
 	FileRequestAsync* exfont = AsyncHandler::RequestFile("ExFont");
 	exfont->SetImportantFile(true);
+	exfont->SetNecessary(false);
 	FileRequestAsync* soundfont = AsyncHandler::RequestFile("easyrpg.soundfont");
 	soundfont->SetImportantFile(true);
 	FileRequestAsync* autorun_ineluki = AsyncHandler::RequestFile("autorun.script");
 	autorun_ineluki->SetImportantFile(true);
+	autorun_ineluki->SetNecessary(false);
 
 	db->Start();
 	tree->Start();
