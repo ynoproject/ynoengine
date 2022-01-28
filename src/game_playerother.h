14555c79537b48c8dc4594ab12e2b3cee70f82cd
Flashfyre <flashfireex@gmail.com>
2022-01-27 21:36:26 -0500

    Revert "Add player fade"
    This reverts commit a60eb8ea6ba86278a3360828f96c4eaf8b86c8ca.

--- src/game_playerother.h
+++ src/game_playerother.h
@@ -11,57 +11,21 @@ using Game_PlayerBase = Game_CharacterDataStorage<lcf::rpg::SavePartyLocation>;
  * game character of other clients
  */
 class Game_PlayerOther : public Game_PlayerBase {
-	public:
-		Game_PlayerOther() : Game_CharacterDataStorage(PlayerOther)
-		{
-			SetDirection(lcf::rpg::EventPage::Direction_down);
-			SetMoveSpeed(4);
-			SetAnimationType(lcf::rpg::EventPage::AnimType_non_continuous);
-		}
+public:
+	Game_PlayerOther() : Game_PlayerBase(PlayerOther)
+	{
+		SetDirection(lcf::rpg::EventPage::Direction_down);
+		SetMoveSpeed(4);
+		SetAnimationType(lcf::rpg::EventPage::AnimType_non_continuous);
+	}
 
-		bool IsMultiplayerVisible();
+	void UpdateNextMovementAction() override {
+		//literally just do nothing
+	}
 
-		void SetMultiplayerVisible(bool mv);
-
-		int GetBaseOpacity();
-
-		void SetBaseOpacity(int bo);
-
-		int GetOpacity() const override;
-
-		void UpdateNextMovementAction() override {
-			//literally just do nothing
-		}
-
-		void Update() {
-			Game_Character::Update();
-		}
-
-	private:
-		bool multiplayer_visible;
-		/* 0 = Invisible, 32 = Opaque */
-		int base_opacity;
+	void Update() {
+		Game_Character::Update();
+	}
 };
-
-inline bool Game_PlayerOther::IsMultiplayerVisible() {
-	return multiplayer_visible;
-}
-
-inline void Game_PlayerOther::SetMultiplayerVisible(bool mv) {
-	multiplayer_visible = mv;
-}
-
-inline int Game_PlayerOther::GetBaseOpacity() {
-	return base_opacity;
-}
-
-inline void Game_PlayerOther::SetBaseOpacity(int bo) {
-	base_opacity = std::clamp(bo, 0, 32);
-}
-
-inline int Game_PlayerOther::GetOpacity() const {
-	float opacity = (float)Game_Character::GetOpacity() * ((float)base_opacity / 32.0);
-	return std::floor(opacity);
-}
 
 #endif
