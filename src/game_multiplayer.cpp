ddb5ad6334ba033520bec7d928583dff3525b39e
Flashfyre <flashfireex@gmail.com>
2022-01-27 21:19:29 -0500

    Attempt to fix crashing bug with player fade

--- src/game_multiplayer.cpp
+++ src/game_multiplayer.cpp
@@ -1,4 +1,3 @@
-#include <list>
 #include <map>
 #include <memory>
 #include <queue>
@@ -130,7 +129,7 @@ namespace {
 	std::string host_nickname = "";
 	std::string key = "";
 	std::map<int, PlayerOther> players;
-	std::list<PlayerOther> dc_players;
+	std::vector<PlayerOther> dc_players;
 	std::queue<std::string> message_queue;
 	const std::string param_delim = "\uffff";
 	const std::string message_delim = "\ufffe";
@@ -838,26 +837,30 @@ void Game_Multiplayer::Update() {
 		p.second.sprite->Update();
 	}
 
-	auto dcpi = dc_players.begin();
-	while (dcpi != dc_players.end()) {
-		auto& ch = (*dcpi).ch;
-		if (ch->GetBaseOpacity() > 0) {
-			ch->SetBaseOpacity(ch->GetBaseOpacity() - 1);
-			ch->SetProcessed(false);
-			ch->Update();
-			(*dcpi).sprite->Update();
-			dcpi++;
-		} else {
-			auto scene_map = Scene::Find(Scene::SceneType::Map);
-			if (scene_map == nullptr) {
-				Output::Debug("unexpected");
-				return;
-			}
-			auto old_list = &DrawableMgr::GetLocalList();
-			DrawableMgr::SetLocalList(&scene_map->GetDrawableList());
-			dcpi = dc_players.erase(dcpi);
-			DrawableMgr::SetLocalList(old_list);
+	if (!dc_players.empty()) {
+		auto scene_map = Scene::Find(Scene::SceneType::Map);
+		if (scene_map == nullptr) {
+			Output::Debug("unexpected");
+			return;
 		}
+
+		auto old_list = &DrawableMgr::GetLocalList();
+		DrawableMgr::SetLocalList(&scene_map->GetDrawableList());
+		
+		auto dcpi = dc_players.rbegin();
+		for (auto dcpi = dc_players.rbegin(); dcpi != dc_players.rend(); dcpi++) {
+			auto& ch = (*dcpi).ch;
+			if (ch->GetBaseOpacity() > 0) {
+				ch->SetBaseOpacity(ch->GetBaseOpacity() - 1);
+				ch->SetProcessed(false);
+				ch->Update();
+				(*dcpi).sprite->Update();
+			} else {
+				dc_players.erase(dcpi.base() - 1);
+			}
+		}
+
+		DrawableMgr::SetLocalList(old_list);
 	}
 
 	if (!message_queue.empty()) {
