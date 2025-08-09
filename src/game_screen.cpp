ynoproject/ynoengine

commit 9c203419af24ca1a770c1eb1efb23b2a36a0e50e

Make other users' repeating flash intensity consistent with the player's
Looks like the flash is a little bit stronger when Flash is called
after Game_Character::Update for the given other player.
Spriteset_Map::Update runs after Game_Multiplayer::Update
(which calls the individual Update methods of other players)
so the call had to be moved to the latter.

src/multiplayer/game_multiplayer.cpp
@@ -823,6 +823,7 @@ void Game_Multiplayer::Update() {
 
 		bool check_chat_name_overlap = frame_index % (8 + ((players.size() >> 4) << 3)) == 0;
 
+		ApplyRepeatingFlashes();
 		for (auto& p : players) {
 			auto& q = p.second.mvq;
 			auto& ch = p.second.ch;

src/spriteset_map.cpp
@@ -96,8 +96,6 @@ void Spriteset_Map::Update() {
 		character_sprite->SetTone(new_tone);
 	}
 
-	GMI().ApplyRepeatingFlashes();
-
 	panorama->SetOx(Game_Map::Parallax::GetX());
 	panorama->SetOy(Game_Map::Parallax::GetY());
 	panorama->SetTone(new_tone);

commit 80d5ace673e97c32e02e3bf53b3bc27853f92b1f

Don't make other players flash on screen flash
It makes no visual difference now that player names are affected
by screen flashes.

Fixes PlayerOther flickering in MAP2059 (Sunset Savannah, Yume
2kki) seen during the event triggered by using the Fairy effect.
Regression introduced in 9c203419af24ca1a770c1eb1efb23b2a36a0e50e.

src/game_screen.cpp
@@ -119,8 +119,6 @@ void Game_Screen::FlashOnce(int r, int g, int b, int s, int frames) {
 	data.flash_time_left = frames;
 	data.flash_continuous = false;
 	flash_period = 0;
-
-	GMI().ApplyFlash(r, g, b, s, frames);
 }
 
 void Game_Screen::FlashBegin(int r, int g, int b, int s, int frames) {

src/multiplayer/game_multiplayer.cpp
@@ -738,13 +738,6 @@ void Game_Multiplayer::ApplyPlayerBattleAnimUpdates() {
 	}
 }
 
-void Game_Multiplayer::ApplyFlash(int r, int g, int b, int power, int frames) {
-	for (auto& p : players) {
-		p.second.ch->Flash(r, g, b, power, frames);
-		p.second.chat_name->SetFlashFramesLeft(frames);
-	}
-}
-
 void Game_Multiplayer::ApplyRepeatingFlashes() {
 	for (auto& rf : repeating_flashes) {
 		if (players.find(rf.first) != players.end()) {

src/multiplayer/game_multiplayer.h
@@ -42,7 +42,6 @@ public:
 	bool IsBattleAnimSynced(int anim_id);
 	void PlayerBattleAnimShown(int anim_id);
 	void ApplyPlayerBattleAnimUpdates();
-	void ApplyFlash(int r, int g, int b, int power, int frames);
 	void ApplyRepeatingFlashes();
 	void ApplyTone(Tone tone);
 	void ApplyScreenTone();