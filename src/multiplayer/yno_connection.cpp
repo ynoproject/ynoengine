9c0d6a2a712161224376baf6d925c72a050243a7
Viet Dinh <54ckb0y789@gmail.com>
2026-05-26 22:21:23 -0400

    Disable CI linter

--- src/multiplayer/yno_connection.cpp
+++ src/multiplayer/yno_connection.cpp
@@ -2,6 +2,10 @@
 #include <emscripten/websocket.h>
 #include "../external/TinySHA1.hpp"
 
+#ifndef PLAYER_PSK
+#define PLAYER_PSK
+#endif
+
 struct YNOConnection::IMPL {
 	EMSCRIPTEN_WEBSOCKET_T socket;
 	uint32_t msg_count;
@@ -160,7 +164,7 @@ std::string as_big_endian_bytes(T v) {
 		return reverse_endian(sr);
 }
 
-const unsigned char psk[] = {};
+const unsigned char psk[] = PLAYER_PSK;
 
 std::string calculate_header(uint32_t key, uint32_t count, std::string_view msg) {
 	std::string hashmsg{as_bytes(psk)};
